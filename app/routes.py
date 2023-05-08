5d352b20ad1de86f52d25107552859eb6d042c89
MidhinV <110954433+MidhinV@users.noreply.github.com>
2023-05-08 19:37:37 +0800

    LogIn/Register

--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,71 @@
+from flask import render_template, flash, redirect, url_for
 from app import app
-from flask import render_template, request, session, redirect, url_for
-from pydantic import BaseModel, validator, ValidationError
-import secrets
+from app.forms import LoginForm
+from flask_login import logout_user
+from flask_login import current_user, login_user
+from app.models import User
+from flask_login import login_required 
+#libraries to redirect a non-logged in user back to the login page when they tries
+#to access the protected index page
+from flask import request
+from werkzeug.urls import url_parse
+from app import db
+from app.forms import RegistrationForm
 
 @app.route('/')
-def home():
-    return render_template('home.html')
+@app.route('/index')
+#making sure a user is logged in to access the index page '@login_required'
+@login_required 
+def index():
+    user = {'username': 'Miguel'}
+    posts = [
+    {
+    'author': {'username': 'John'},
+    'body': 'Beautiful day in Portland!'
+    },
+    {
+    'author': {'username': 'Susan'},
+    'body': 'The Avengers movie was so cool!'
+    }
+    ]
+    return render_template("index.html", title='Home Page', posts=posts)
+@app.route('/button')
+def button():
+    return render_template("button.html", title="Button!")
 
-@app.route('/chatbox/')
-def chat():
-    return render_template('chatbox.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
 
-@app.route('/profile/')
-def profile():
-    return render_template('profile.html')
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
-
-
-if __name__ =='__main__':
-    app.run(debug=True)
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
