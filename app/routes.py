ec1e8db15f43b0a92232652f0632fd6751758e0f
Sahil Narula <126839546+Hunkhill1@users.noreply.github.com>
2023-05-09 21:26:35 +0800

    Went half way through chapter 3 will pcik up later megaflask sah

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -18,6 +19,10 @@ def index():
             'author':{'username':'Jenga'},
             'body': 'I do believe everyone should understand RegEX'
         }
-
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
