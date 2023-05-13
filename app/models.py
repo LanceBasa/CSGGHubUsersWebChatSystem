8f309682de4d241883d0cb72a77357a150b02384
MidhinV <110954433+MidhinV@users.noreply.github.com>
2023-05-13 13:40:14 +0800

    updated database

--- app/models.py
+++ app/models.py
@@ -13,23 +13,84 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    rank_id = db.Column(db.Integer, db.ForeignKey('rank.id'))
+    chat = db.relationship('Chat', backref='author', lazy='dynamic')
+    created_rooms = db.relationship('ChatRoom', backref='creator', lazy='dynamic')
+    fav_weapons = db.relationship('FavWeapon', backref='user', lazy='dynamic')
+    fav_maps = db.relationship('FavMap', backref='user', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
-    
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-class Post(db.Model):
+class Chat(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    text = db.Column(db.String(500))
+    created_at = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    room_id = db.Column(db.Integer, db.ForeignKey('chatroom.id'))
+
+    def __repr__(self):
+        return '<Chat {}>'.format(self.text)
+
+class ChatRoom(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    room_name = db.Column(db.String(64), unique=True)
+    created_by = db.Column(db.Integer, db.ForeignKey('user.id'))
+    chat = db.relationship('Chat', backref='room', lazy='dynamic')
+
+    def __repr__(self):
+        return '<ChatRoom {}>'.format(self.room_name)
+
+class RoomUser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    room_id = db.Column(db.Integer, db.ForeignKey('chatroom.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
-        return '<Post {}>'.format(self.body)
-    
+        return '<RoomUser {}>'.format(self.id)
+
+class FavWeapon(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    weapon_id = db.Column(db.Integer, db.ForeignKey('weapon.id'))
+
+    def __repr__(self):
+        return '<FavWeapon {}>'.format(self.id)
+
+class Weapon(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    weapon_name = db.Column(db.String(64), unique=True)
+    users = db.relationship('FavWeapon', backref='weapon', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Weapon {}>'.format(self.weapon_name)
+
+class FavMap(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    map_id = db.Column(db.Integer, db.ForeignKey('map.id'))
+
+    def __repr__(self):
+        return '<FavMap {}>'.format(self.id)
+
+class Map(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    map_name = db.Column(db.String(64), unique=True)
+    users = db.relationship('FavMap', backref='map', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Map {}>'.format(self.map_name)
+
+class Rank(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    rank_name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='rank', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Rank {}>'.format(self.rank_name)
