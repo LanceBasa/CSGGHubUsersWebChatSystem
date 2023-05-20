944435e4b814f3304a0f1b64bd1e109f12e99a6c
LanceBasa <lance_olisco@yahoo.com>
2023-05-20 17:07:00 +0800

    most commands working

--- app/sockets.py
+++ app/sockets.py
@@ -7,43 +7,23 @@ from sqlalchemy import desc
 from flask_login import current_user
 from sqlalchemy.orm import joinedload
 
+#System broadcast that a user has joined the chatroom
+@socketio.on('join', namespace='/chat')
+def handle_user_join(data):
+    # Retrieve the current room from the session
+    # Join the room
+    room = session.get("chat")
+
+    # Join the room
+    join_room(room)
+    print('Client connected!', file=sys.stderr)
+
+    username = current_user.username  # Assuming you have the username of the current user
+    join_message = f"User '{username}' has joined the chat"
+    emit('chat', {'message': join_message, 'username' :'System'}, room=room, broadcast=True)
 
 
-
-
-
-# for adding entered message in the chat to the database and emitting it back to chatbox
-@socketio.on("new_message", namespace='/chat')
-def handle_new_message(message):
-    if current_user.is_authenticated:
-        room = session.get("chat")
-        # Create a new Chat object and store it in the database
-        chat_message = Chat(text=message, user_id=current_user.id)
-
-        db.session.add(chat_message)
-        db.session.commit()
-
-        if message[0]=='~':
-            print('it is ~')
-            comm=message[1:]
-            comm=comm.split()
-            item=""
-            if(len(comm)>=2):
-                item=comm[1]
-            command = Commands.query.filter_by(command_name=comm[0]).first()
-            comm=command.query_command
-            comm=eval(comm)
-
-            emit("chat", {"message": message, "username": current_user.username}, broadcast=True, room=room)
-            emit("chat", {"message": comm, "username": 'System'}, broadcast=True, room=room)
-        else:
-            print(f"New message: {message}", file=sys.stderr)
-            emit("chat", {"message": message, "username": current_user.username}, broadcast=True, room=room)
-    else:
-        print("Error: Unauthenticated user tried to send a message", file=sys.stderr)
-
-
-
+#socket for loading the chat history in the chatbox
 @socketio.on('connect', namespace='/chat')
 def handle_connect(data):
     # Retrieve the current room from the session
@@ -61,53 +41,85 @@ def handle_connect(data):
     # Send the messages to the client
     emit('load_messages', messages)
 
-    # This assumes that you want to send a status message every time a client connects
-    emit('status', {'msg': 'New client connected!'}, room=room)
 
 
-@socketio.on('join', namespace='/chat')
-def handle_user_join(data):
-    # Retrieve the current room from the session
-    # Join the room
-    room = session.get("chat")
+# for adding entered message in the chat to the database and emitting it back to chatbox
+@socketio.on("new_message", namespace='/chat')
+def handle_new_message(message):
+    if current_user.is_authenticated:
+        room = session.get("chat")
+        
+        #strip chat leading and trailing spaces
+        message=message.strip()
 
-    # Join the room
-    join_room(room)
-    print('Client connected!', file=sys.stderr)
+        # Create a new Chat object and store it in the database
+        chat_message = Chat(text=message, user_id=current_user.id)
 
-    username = current_user.username  # Assuming you have the username of the current user
-    join_message = f"User '{username}' has joined the chat"
-    emit('chat', {'message': join_message, 'username' :'System'}, room=room, broadcast=True)
+        db.session.add(chat_message)
+        db.session.commit()
+
+        #check if the message is a command if so
+        if message[0]=='~':
+            print('it is ~')
+            comm=message[1:]
+            comm=comm.split()
+            item=""
+            if(len(comm)>=2):
+                item=comm[1]
+                
+            command = Commands.query.filter_by(command_name=comm[0]).first()
+            if command: 
+                comm=eval(command.query_command)
+            else:
+                comm="command not found"
+            emit("chat", {"message": message, "username": current_user.username}, broadcast=True, room=room)
+            emit("chat", {"message": comm, "username": 'System'}, broadcast=True, room=room)
+        else:
+            print(f"New message: {message}", file=sys.stderr)
+            emit("chat", {"message": message, "username": current_user.username}, broadcast=True, room=room)
+    else:
+        print("Error: Unauthenticated user tried to send a message", file=sys.stderr)
 
 
 
+#------------- for running special commands---------------------------------
 
-#------- for special commands-----------------
+
+
 def get_user_fav_weapons(username):
     user = User.query.filter_by(username=username).first()
     if user:
         fav_weapons = (
-            FavWeapon.query
-            .join(Weapon)
-            .options(joinedload(FavWeapon.weapon))
-            .filter_by(user_id=user.id)
+            db.session.query(Weapon.weapon_name)
+            .join(FavWeapon)
+            .filter(FavWeapon.user_id == user.id)
             .all()
         )
-        return [fav_weapon.weapon.weapon_name for fav_weapon in fav_weapons]
+        user = [fav_weapon.weapon_name for fav_weapon in fav_weapons]
+        return " ".join(user)
     return 'Username not found'
 
 def get_all_weapon_names():
-    weapon_names = Weapon.query.with_entities(Weapon.name).all()
-    return [name for (name,) in weapon_names]
+    weapon_names = Weapon.query.all()
+    weapon_names=[weapon.weapon_name for weapon in weapon_names]
+    return " ".join(weapon_names)
+
 
 def get_weapons_by_category(category):
     weapons = Weapon.query.filter_by(category=category).all()
-    return weapons
+    if not weapons:
+        weapons = Weapon.query.all()
+        weapons = set(weapon.category for weapon in weapons)
+        return " Categories available"+ str(weapons)
+    weapons= [weapon.weapon_name for weapon in weapons]
+    return " ".join(weapons)
 
 
 def get_all_maps():
     maps = Map.query.all()
-    return maps
+    maps =[map.map_name for map in maps]
+    return ('\n'.join(maps))
+     
 
 def get_weapon_by_name(name):
     weapon = Weapon.query.filter_by(weapon_name=name).first()
